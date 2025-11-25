0cb27cd1c4aa580efc9900f6fa080925a1391d86
drolejan <drolejan@gmail.com>
2025-11-25 10:24:23 -0600

    iñakiSichambeo

--- Assets/Scripts/RaycastShoot.cs
+++ Assets/Scripts/RaycastShoot.cs
@@ -4,6 +4,7 @@ public class RaycastShoot : MonoBehaviour
 {
     public Camera cam;     // Cámara desde donde saldrá el raycast
     public float range = 100f;
+    public GameObject hitEffect;
 
     void Update()
     {
@@ -19,11 +20,14 @@ public class RaycastShoot : MonoBehaviour
         // Ray desde el centro de la pantalla
         Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
         RaycastHit hit;
+        //Debug.DrawRay(transform.position,Vector3.forward*100);
 
         if (Physics.Raycast(ray, out hit, range))
         {
             // Por ahora solo mostramos qué se golpeó
             Debug.Log("Golpeaste: " + hit.collider.name);
+            GameObject balaeffect = Instantiate(hitEffect,hit.point,Quaternion.identity);
+            Destroy(balaeffect,1f);//Esto destruye el impacto despues de 1 seg
 
             // Ejemplo para práctica:
             // si el objeto tiene tag "Enemy", lo destruimos
